c27e4bc58926808a7789efc780d6a94d21e76615
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2023-06-01 21:11:13 +0300

    init commit

---
+++ src/main/java/com/example/justmarket/JustMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.justmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustMarketApplication.class, args);
+    }
+
+}
